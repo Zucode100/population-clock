3bb640844bf401cb0b49a852b08b6f9d74d6f422
Zucode100 <158639532+Zucode100@users.noreply.github.com>
2024-02-02 20:56:35 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// Save data to local storage
+localStorage.setItem('populationData', JSON.stringify(85348562));
+
+// Retrieve data from local storage
+var populationData = JSON.parse(localStorage.getItem('populationData'));
